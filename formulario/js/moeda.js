4a1248d378845b1bd1c7b2a1e7fea82aef3b0208
Rayanne Franco <160090372+Annfranco@users.noreply.github.com>
2025-09-10 11:19:42 -0300

    Add files via upload

---
+++ formulario/js/moeda.js
@@ -0,0 +1,11 @@
+const real = document.querySelector('#real')
+const botao = document.querySelector('#botao')
+const resultado = document.querySelector('#resultado')
+
+botao.addEventListener('click', calcular_moeda)
+
+function calcular_moeda(){
+    valor = Number(real.value)
+    dolar = valor/5.41
+    resultado.textContent /*imprimir em algo vazio*/ = `O valor em dolar é US$ ${dolar.toFixed(2)}`
+}
